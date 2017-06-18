fe5eb5d799b3d15e20ce00514b7302f93f5eb018
Vijay Garimella <garimella.vijay@gmail.com>
2017-06-17 23:43:30 -0500

    Added TestApp sample console project to demonstrate calling the API using autogenerated client classes.

---
+++ SwaggerWithWebApi/ClientApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Rest;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static ClientAppClient NewAPIClient()
+        {
+            var client = new ClientAppClient(new Uri(ConfigurationManager.AppSettings["API_URI"]));
+
+        }
+    }
+}
